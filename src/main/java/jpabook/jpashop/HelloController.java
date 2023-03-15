27eeab370755ebb9c0be3f6eb1e369ba37642515
ynyejn <yeounshi@gmail.com>
2023-03-15 22:59:09 +0900

    view환경설정 및 devtools의존성추가

---
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hello1!!");
+        return "hello";//화면이름 hello.html로내보냄
+    }
+}
