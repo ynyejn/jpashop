e6fd046136341ee794c49ed4124a289054f5b4c3
ynyejn <yeounshi@gmail.com>
2023-03-29 17:25:30 +0900

    uses unchecked or unsafe operations 에러

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -27,7 +27,7 @@ public class MemberApiController {
         List<Member> members = memberService.findMembers();
         List<MemberDto> collect = members.stream().map(m -> new MemberDto(m.getName()))
                 .collect(Collectors.toList());
-        return new Result(collect.size(),collect);
+        return new Result<>(collect.size(),collect);
 
     }
 
